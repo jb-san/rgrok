40e07699cd983e1b93c74bede0521a5dfa892f8b
Jonathan Borg <jonathan@x3m.io>
2019-12-05 19:44:27 +0100

    trying stuff out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod rgrokCLI;
+
 extern crate clap;
 use clap::{App, Arg, SubCommand};
 
@@ -5,43 +7,42 @@ fn main() {
     let matches = App::new("rgrok")
         .version("1.0")
         .author("Jonathan Borg <jonathan@x3m.io>")
-        .about("Create a tunnel for your local http(s) server to the internet")
-        .args_from_usage(
-            "-c, --config=[FILE] 'Sets a custom config file'
-                              <INPUT>              'Sets the input file to use'
-                              -v...                'Sets the level of verbosity'",
+        .about("Create a tunnel to your local dev server")
+        .before_help("")
+        .arg(
+            Arg::with_name("hostname")
+                .short("h")
+                .long("hostname")
+                .value_name("HOSTNAME")
+                .help("The host to your server that is running rgrokd")
+                .required(true)
+                .takes_value(true),
         )
-        .subcommand(
-            SubCommand::with_name("test")
-                .about("controls testing features")
-                .version("1.3")
-                .author("Someone E. <someone_else@other.com>")
-                .arg_from_usage("-d, --debug 'Print debug information'"),
+        .arg(
+            Arg::with_name("httpauth")
+                .short("a")
+                .long("httpauth")
+                .value_name("HTTPAUTH")
+                .help("user:password")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("PORT")
+                .help("Port of your local dev server")
+                .required(true)
+                .index(1),
         )
         .get_matches();
-    let config = matches.value_of("config").unwrap_or("default.conf");
-    println!("Value for config: {}", config);
 
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
-
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match matches.occurrences_of("v") {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
+    // // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+    // // required we could have used an 'if let' to conditionally get the value)
+    // println!("Using input file: {}", matches.value_of("INPUT").unwrap());
 
     // You can handle information about subcommands by requesting their matches by name
     // (as below), requesting just the name used, or both at the same time
-    if let Some(matches) = matches.subcommand_matches("test") {
-        if matches.is_present("debug") {
-            println!("Printing debug info...");
-        } else {
-            println!("Printing normally...");
-        }
-    }
+    let hostname = String::from(matches.value_of("HOSTNAME").unwrap());
+    let httpauth = String::from(matches.value_of("HTTPAUTH").unwrap());
+    rgrokCLI::connect(hostname, httpauth);
+    println!("{:?}", matches)
 }
---
+++ src/rgrokCLI.rs
@@ -0,0 +1,4 @@
+pub fn connect(hostname: String, httpauth: String) -> () {
+    println!("Hostname: {}", hostname);
+    println!("auth: {}", httpauth);
+}
